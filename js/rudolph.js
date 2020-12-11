f11316cab4a3ab31fd64172f454c6f5876a7709b
Jongyoon Jeong <wormwlrm@naver.com>
2020-12-11 16:28:30 +0900

    fix: 불필요한 코드 삭제

--- js/rudolph.js
+++ js/rudolph.js
@@ -1,19 +1,13 @@
 (function () {
   "use strict";
 
-  const WATCHED_KEY = 'rudolph-watched';
   const REJECTED_KEY = 'rudolph-rejected';
-  
-  const watched = localStorage.getItem(WATCHED_KEY);
+
   const rejected = localStorage.getItem(REJECTED_KEY);
 
   $('#rudolph .reject').on('click', function (e) {
     e.preventDefault();
     localStorage.setItem(REJECTED_KEY, 'on')
-  })
-
-  $('#rudolph .watch').on('click', function (e) {
-    e.preventDefault();
   })
 
   if (!rejected) {
